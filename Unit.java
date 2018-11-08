944cf04014be458ae882ea904632ab448bfe1f7f
CrowVendor <wmaxonbremer@gmail.com>
2018-11-07 21:14:25 -0600

    worked on feature level and instantiation

--- Unit.java
+++ Unit.java
@@ -1,20 +1,13 @@
 public class Unit{
-  private ArrayList<Unit> connections;
-  private boolean on;
+  private ArrayList<doubles> connections;
   public Unit(int level){
     on = false;
   }
-  public void addConnection(Unit connect){
+  public void addConnection(double connect){
     connections.append(connect);
   }
   public ArrayList<Unit> getConnections(){
     return connnections;
-  }
-  public void turnOn(){
-    on = true;
-  }
-  public boolean isOn(){
-    return on;
   }
 
 }
